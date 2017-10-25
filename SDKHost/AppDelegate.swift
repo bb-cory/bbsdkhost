41a47dd4bd403e5c654f771838ad98f1100de353
Cory Alder <cory@buddybuild.com>
2017-10-25 16:02:27 -0700

    Clear user defaults on launch

--- SDKHost/AppDelegate.swift
+++ SDKHost/AppDelegate.swift
@@ -15,9 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if let bundle = Bundle.main.bundleIdentifier, ProcessInfo.processInfo.arguments.contains("reset_user_defaults") {
+            UserDefaults.standard.removePersistentDomain(forName: bundle)
+        }
+        
         BuddyBuildSDK.setup()
         
-        // Override point for customization after application launch.
         return true
     }
 
